cceab068be39429110f65596ad6296187dc97194
Bin Guo <guobin201314@gmail.com>
2019-04-17 15:49:55 +0800

    配置

--- vue.config.js
+++ vue.config.js
@@ -1,55 +1,40 @@
-
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const CompressionWebpackPlugin = require('compression-webpack-plugin');
-const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i;
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+// const CompressionWebpackPlugin = require('compression-webpack-plugin');
+// const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i;
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-    productionSourceMap: isProd ,
-    pluginOptions: {},
-    configureWebpack: config => {
-        if (isProd) {
-            const plugins = [];
-            plugins.push(
-                new UglifyJsPlugin({
-                    uglifyOptions: {
-                        compress: {
-                            warnings: false,
-                            drop_console: true,
-                            drop_debugger: false,
-                            pure_funcs: ['console.log']//移除console
-                        }
-                    },
-                    sourceMap: false,
-                    parallel: true
-                })
-            );
-            plugins.push(
-                new CompressionWebpackPlugin({
-                    filename: '[path].gz[query]',
-                    algorithm: 'gzip',
-                    test: productionGzipExtensions,
-                    threshold: 10240,
-                    minRatio: 0.8
-                })
-            );
-            config.plugins = [
-                ...config.plugins,
-                ...plugins
-            ];
-        }
-    },
-    chainWebpack: config => {
-        config.module
-            .rule("images")
-            .use("image-webpack-loader")
-            .loader("image-webpack-loader")
-            .options({
-                mozjpeg: { progressive: true, quality: 65 },
-                optipng: { enabled: false },
-                pngquant: { quality: "65-90", speed: 4 },
-                gifsicle: { interlaced: false },
-                webp: { quality: 75 }
-            });
-    }
+    productionSourceMap: false,
+    // configureWebpack: config => {
+    //     if (isProd) {
+    //         const plugins = [];
+    //         plugins.push(
+    //             new UglifyJsPlugin({
+    //                 uglifyOptions: {
+    //                     compress: {
+    //                         warnings: false,
+    //                         drop_console: true,
+    //                         drop_debugger: false,
+    //                         pure_funcs: ['console.log']//移除console
+    //                     }
+    //                 },
+    //                 sourceMap: false,
+    //                 parallel: true
+    //             })
+    //         );
+    //         plugins.push(
+    //             new CompressionWebpackPlugin({
+    //                 filename: '[path].gz[query]',
+    //                 algorithm: 'gzip',
+    //                 test: productionGzipExtensions,
+    //                 threshold: 10240,
+    //                 minRatio: 0.8
+    //             })
+    //         );
+    //         config.plugins = [
+    //             ...config.plugins,
+    //             ...plugins
+    //         ];
+    //     }
+    // }
 };
