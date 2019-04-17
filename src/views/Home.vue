138b1c7caa3144a84c12d5feaec46bdf71e5b8c8
Bin Guo <guobin201314@gmail.com>
2019-04-17 09:07:15 +0800

    views

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="home">
+        <p>class name</p>
+        <el-button type="primary" @click="openModal">button</el-button>
+        <el-select :value="1"></el-select>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from "vue-property-decorator";
+
+    @Component({
+        components: {},
+    })
+    export default class Home extends Vue {
+        public openModal() {
+            this.$message.info('open message');
+            this.$notify.info('notify');
+            this.$message.warning('warn');
+            const loading =this.$loading({
+                lock: true,
+                text: 'Loading',
+                spinner: 'el-icon-loading',
+                background: 'rgba(0, 0, 0, 0.7)'
+            });
+
+            setTimeout(() => {
+                loading.close();
+            }, 2000);
+        }
+    }
+</script>
---
+++ src/views/VuexPage.vue
@@ -0,0 +1,51 @@
+<template>
+
+    <div>
+        <div>
+            <h1>{{author.name}}</h1>
+        </div>
+        <div>
+            <form>
+                <!-- 同步改变store数据-->
+                <label for="name">name: </label>
+                <input id="name" type="text" placeholder="输入名字" v-model="author.name">
+
+                <label for="name1">name: </label>
+                <input id="name1" type="text" placeholder="输入名字1" v-model="myAuthor.name">
+
+            </form>
+            <button type="button" @click="changeName">更改store</button>
+        </div>
+    </div>
+
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+
+    import { Author } from '@/store/types';
+    import { Getter, Mutation } from 'vuex-class';
+
+    @Component({
+        components: {},
+    })
+    export default class VuexPage extends Vue {
+
+        public myAuthor: Author = new Author('');
+
+        @Getter('author') public author: any;
+        // Mutation
+        @Mutation public setAuthor: any;
+
+        public changeName() {
+            // console.log(this.$store.state.author);
+            // console.log(this.myAuthor);
+            this.setAuthor(this.myAuthor);
+            // console.log(this.$store.state.author);
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
