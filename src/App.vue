1f8cb69f0c3e73d4de8e3eb11175684e10c25c6b
Bin Guo <guobin201314@gmail.com>
2019-04-17 15:49:40 +0800

    主路由

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,40 @@
 <template>
-  <div id="app">
-    <m-header></m-header>
-    <keep-alive>
-      <router-view/>
-    </keep-alive>
-    <m-footer></m-footer>
-    <m-player></m-player>
-  </div>
+    <div id="app">
+        <MHeader></MHeader>
+        <keep-alive>
+            <router-view/>
+        </keep-alive>
+        <MFooter></MFooter>
+        <MPlayer></MPlayer>
+    </div>
 </template>
 <script lang="ts">
-  import { Component, Vue } from 'vue-property-decorator';
-  import MHeader from '@/common/MHeader.vue';
-  import MFooter from '@/common/MFooter.vue';
-  import MPlayer from '@/common/MPlayer.vue';
+    import { Component, Vue } from "vue-property-decorator";
+    import MFooter from "@/common/m-footer/MFooter.vue";
+    import MHeader from "@/common/m-header/MHeader.vue";
+    import MPlayer from "@/common/m-player/MPlayer.vue";
 
-  @Component({
-    components: {
-      MHeader,
-      MFooter,
-      MPlayer,
-    },
-  })
-  export default class App extends Vue {
-
-  }
+    @Component({
+        components: {
+            MHeader,
+            MFooter,
+            MPlayer,
+        },
+    })
+    export default class App extends Vue {
+    }
 </script>
-<style lang="stylus">
-  @import "styles/normalize.css";
-  html, body
-    margin 0
-    width 100%
-    height 100%
-    font-family 'PingFang SC', 'STHeitiSC-Light', 'Helvetica-Light', arial, sans-serif, 'Droid Sans Fallback'
-    -webkit-font-smoothing: antialiased
-    -moz-osx-font-smoothing grayscale
-    user-select none
-    -webkit-tap-highlight-color transparent
-    #app
-      width 100%
-      height 100%
-      margin 0
-      overflow-x hidden
-      overflow-y scroll
-      color #2c3e50
+<style lang="scss">
+    @import "styles/@color";
+    body, html{
+        line-height: 1;
+        width: 100%;
+        height: 100%;
+        font-family: 'PingFang SC', 'STHeitiSC-Light', 'Helvetica-Light', arial, sans-serif, 'Droid Sans Fallback';
+        user-select: none;
+        -webkit-tap-highlight-color: transparent;
+        background: $color-background;
+        color: $color-text;
+    }
+
 </style>
