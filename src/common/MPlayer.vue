e50b162e7e83f33dbb5e51c14ca1838aa8a74bb7
Bin Guo <guobin201314@gmail.com>
2019-04-17 09:08:02 +0800

    公共组件

---
+++ src/common/MPlayer.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <div>
+        <h1>player</h1>
+    </div>
+
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+    @Component
+    export default class MPlayer extends Vue {
+    }
+</script>
+
+<style scoped>
+
+</style>
